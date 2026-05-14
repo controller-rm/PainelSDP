978fefbac811a2c0d99fcecfe24914712c7f1d11
Controller <controller.rem@gmail.com>
2026-05-13 22:50:51 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,28 @@
-import os
 import mysql.connector
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
-
-def get_connection():
+def conectar_erp():
     return mysql.connector.connect(
         host=MYSQL_HOST,
         port=MYSQL_PORT,
         user=MYSQL_USER,
         password=MYSQL_PASSWORD,
-        database=MYSQL_DATABASE,
+        database="SOFTDIB_ADEX"
+    )
+
+def conectar_painel():
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database="adex"
     )
